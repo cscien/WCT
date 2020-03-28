d8765acd4c1e2d76000cdf39c6a26ec674d3e1fb
cscien <cscien@163.com>
2020-03-28 21:00:31 +0800

    增加了‘关于本网站’
    添加了function 和网站网页

--- wordcount/function.py
+++ wordcount/function.py
@@ -9,8 +9,9 @@ def count(request):
 	user_text=request.GET['text']		#定义变量user_text
 	total_count=len(user_text)			#定义变量total_count
 	word_dict = {}						#定义字典变量变量
+	sorted_dict = {}					#定义字典变量并赋值
 
-	print(total_count)
+	#print(total_count)
 
 	for word in user_text:
 		if word not in word_dict:
@@ -18,8 +19,11 @@ def count(request):
 		else:
 			word_dict[word] += 1
 	sorted_dict = sorted(word_dict.items(),\
-		    key = lambda w: w[1], reverse = True)	#定义字典变量并赋值
+		    key = lambda w: w[1], reverse = True)	
 	return render(request,'count.html',
 		{'cnt':total_count,'txt':user_text,
 		'wddict': word_dict,'srt': sorted_dict})
 	
+
+def about(request):
+	return render(request,'about.html')
